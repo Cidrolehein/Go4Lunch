d1044e80ff42af0e45a25d769f60f8ee52bcc037
Julien Gacon <gaconjulien@gmail.com>
2019-04-16 01:27:41 +0200

    Add Toolbar

--- app/src/main/java/com/gacon/julien/go4lunch/controller/utils/auth/ProfileActivity.java
+++ app/src/main/java/com/gacon/julien/go4lunch/controller/utils/auth/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.gacon.julien.go4lunch.controller.utils.auth;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.gacon.julien.go4lunch.R;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import androidx.appcompat.widget.Toolbar;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -16,11 +20,43 @@ public class ProfileActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+        // Configuring Toolbar
+        this.configureToolbar();
     }
 
+    /**
+     * Log Out Button
+     */
     @OnClick(R.id.button_log_out)
     public void onClickSignOutButton() {
         this.signOutUserFromFirebase();
+    }
+
+    /**
+     * Inflate the menu and add it to the Toolbar
+     * @param menu activity menu
+     * @return the menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+
+    /**
+     * Handle actions on menu items
+     * @param item search
+     * @return item selected
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "No search", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     // --------------------
@@ -41,8 +77,6 @@ public class ProfileActivity extends BaseActivity {
     // UI
     // --------------------
 
-    // - Create OnCompleteListener called after tasks ended
-
     /**
      * Create OnCompleteListener called after tasks ended
      * @return is REST Resquests completed ?
@@ -52,6 +86,16 @@ public class ProfileActivity extends BaseActivity {
             finish();
             startLoginSignInActivity();
         };
+    }
+
+    /**
+     * Add Toolbar
+     */
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 
 }

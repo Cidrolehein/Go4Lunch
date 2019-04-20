4ad6fb7602d669beace3e7268b4529cf45d7b9c2
Julien Gacon <gaconjulien@gmail.com>
2019-04-20 12:31:04 +0200

    Change recyclerview linearlayout to wrapcontent

--- app/src/main/java/com/gacon/julien/go4lunch/controller/fragments/ListView.java
+++ app/src/main/java/com/gacon/julien/go4lunch/controller/fragments/ListView.java
@@ -26,7 +26,7 @@ public class ListView extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_view, container, false);
+        return inflater.inflate(R.layout.activity_profile, container, false);
     }
 
 }

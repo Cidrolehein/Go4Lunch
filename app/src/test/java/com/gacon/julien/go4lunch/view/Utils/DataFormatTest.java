82db822d66aeffebfdd4e7e229b65b6995d25dd6
Julien Gacon <gaconjulien@gmail.com>
2019-05-05 22:43:25 +0200

    UnitTest begin

---
+++ app/src/test/java/com/gacon/julien/go4lunch/view/Utils/DataFormatTest.java
@@ -0,0 +1,24 @@
+package com.gacon.julien.go4lunch.view.Utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataFormatTest {
+
+    @Test
+    public void formatMeters() {
+    }
+
+    @Test
+    public void getRatingStar() {
+    }
+
+    @Test
+    public void addImages() {
+    }
+
+    @Test
+    public void getDayOpen() {
+    }
+}
